374cb7eb2900a7ad5906422be23fc9440fc22e9d
KevinGacon <48351155+KevinGacon@users.noreply.github.com>
2020-01-24 14:21:43 +0100

    Add files via upload

--- ICT133 - Exercice/SNOW/model/model.php
+++ ICT133 - Exercice/SNOW/model/model.php
@@ -2,15 +2,27 @@
 function checkLogin($data)
 {
     @$current_file = file_get_contents("model/dataBase.json");
-    $parsed = json_decode($current_file);
-    foreach ($parsed as $item => $value)
+    $obj = json_decode($current_file);
+    foreach ($obj as $item => $value)
     {
         if ($value->User == $data['uname'])
         {
             if (@$data['psw'] == $value->Password && (@$data['uname'] == $value->User))
                 return true;
+
             else
                 return false;
         }
     }
+}
+
+function dataBaseShop()
+{
+    /** @var  $current_file * transform data of json file to variable php */
+    $current_file = file_get_contents("model/dataBaseShop.json");
+    /** affected @var $parsed_snowData * with decoding of content of @var $current_file  */
+    $parsed_snowData = json_decode($current_file);
+    /** @var $array_data * this array content element of snowDatabase  */
+    $array_data[] = $parsed_snowData;
+    return $array_data;/** return a snow element  */
 }
